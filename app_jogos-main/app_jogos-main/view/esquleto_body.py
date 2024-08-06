e45b40ca01fd9568a6b113417d47f8012bb83b74
Mateusdorvis004 <mateusdorvis004@gmail.com>
2024-08-06 15:43:52 -0300

    ssds

--- app_jogos-main/app_jogos-main/view/esquleto_body.py
+++ app_jogos-main/app_jogos-main/view/esquleto_body.py
@@ -2,14 +2,19 @@ from tkinter import ttk
 import tkinter as tk
 from elementos_tkinter import Buttoncustomizado, Framecustomizado, Labelcustomizada, LabelcustomizadaTitulo, Mensagens
 class Header:
-    
+    pass
 
 class Body:
     def __init__(self, root):
         self.root = root
-    
+        self.container_frame = Framecustomizado(self.root, bg='red', width=600, height=600)
+        self.container_frame.grid_propagate(False)
+        self.container_frame.grid(row=0, column=0)
+        
+        
     def card_frame(self):
-        pass
+        self.card = Framecustomizado(self.container_frame)
+        self.card.grid(row=0, column=0)
         
     
     
