ab9ffd36374957356eaf4fe8a03f19698554188e
Mateusdorvis004 <mateusdorvis004@gmail.com>
2024-08-06 16:04:49 -0300

    çlçl

--- app_jogos-main/app_jogos-main/view/esquleto_body.py
+++ app_jogos-main/app_jogos-main/view/esquleto_body.py
@@ -1,24 +1,36 @@
 from tkinter import ttk
 import tkinter as tk
 from elementos_tkinter import Buttoncustomizado, Framecustomizado, Labelcustomizada, LabelcustomizadaTitulo, Mensagens
-from images import CarregaImagens
+
 class Header:
     pass
 
+class CardFrame:
+    def __init__(self, frame):
+        self.frame = frame
+    
+    def card(self):
+        self.card = Framecustomizado(self.frame, width=200, height=300, bg='blue')
+        self.card.grid(row=0, column=0, pady=5, padx=5)
 class Body:
     def __init__(self, root):
         self.root = root
         self.container_frame = Framecustomizado(self.root, bg='red', width=600, height=600)
         self.container_frame.grid_propagate(False)
         self.container_frame.grid(row=0, column=0)
-        self.card_frame()
+        self.card_frame1()
         
         
-    def card_frame(self):
-        self.card = Framecustomizado(self.container_frame, width=200, height=300, bg='blue')
-        self.card.grid(row=0, column=0)
-        self.img = CarregaImagens("imagens/bloons.jpg", self.card)
-        self.img.aparecer_imagem(0,0)
+    def card_frame1(self):
+        self.oi = CardFrame(self.container_frame)
+        self.oi2 = CardFrame(self.container_frame)
+        pass
+    
+    def card_frame1(self):
+        self.oi = CardFrame(self.container_frame)
+        self.oi2 = CardFrame(self.container_frame)
+        pass
+        
         
     
     
