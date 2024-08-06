e4eaeb2b910424b38fff415246b29849327ea79a
Mateusdorvis004 <mateusdorvis004@gmail.com>
2024-08-06 16:09:08 -0300

    jkjkjk

--- app_jogos-main/app_jogos-main/view/esquleto_body.py
+++ app_jogos-main/app_jogos-main/view/esquleto_body.py
@@ -6,31 +6,25 @@ class Header:
     pass
 
 class CardFrame:
-    def __init__(self, frame):
+    def __init__(self, frame, linha_card, coluna_card):
         self.frame = frame
-    
-    def card(self):
-        self.card = Framecustomizado(self.frame, width=200, height=300, bg='blue')
-        self.card.grid(row=0, column=0, pady=5, padx=5)
+        self.card_box = Framecustomizado(self.frame, width=200, height=300, bg='blue')
+        self.card_box.grid(row=linha_card, column=coluna_card, pady=5, padx=5)
 class Body:
     def __init__(self, root):
         self.root = root
         self.container_frame = Framecustomizado(self.root, bg='red', width=600, height=600)
         self.container_frame.grid_propagate(False)
         self.container_frame.grid(row=0, column=0)
-        self.card_frame1()
+        self.card_frame()
         
         
-    def card_frame1(self):
-        self.oi = CardFrame(self.container_frame)
-        self.oi2 = CardFrame(self.container_frame)
+    def card_frame(self):
+        CardFrame(self.container_frame, 0, 0)
+        CardFrame(self.container_frame, 0, 1)
+        CardFrame(self.container_frame, 0, 2)
         pass
     
-    def card_frame1(self):
-        self.oi = CardFrame(self.container_frame)
-        self.oi2 = CardFrame(self.container_frame)
-        pass
-        
         
     
     
