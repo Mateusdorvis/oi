d9422a33540c2e5f8c7a4840c91c635285b9e5a9
Mateusdorvis004 <mateusdorvis004@gmail.com>
2024-08-06 15:40:06 -0300

    SDS

--- app_jogos-main/app_jogos-main/view/esquleto_body.py
+++ app_jogos-main/app_jogos-main/view/esquleto_body.py
@@ -1,2 +1,26 @@
 from tkinter import ttk
 import tkinter as tk
+from elementos_tkinter import Buttoncustomizado, Framecustomizado, Labelcustomizada, LabelcustomizadaTitulo, Mensagens
+class Header:
+    
+
+class Body:
+    def __init__(self, root):
+        self.root = root
+    
+    def card_frame(self):
+        pass
+        
+    
+    
+class App:
+    def __init__(self, root):
+        self.root = root
+        self.body()
+    
+    def body(self):
+        pass
+        
+        
+        
+    
